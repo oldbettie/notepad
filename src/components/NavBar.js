14c2f7079877d90b26086b8579d62eb6b0631432
oldbettiegaming@gmail.com <oldbettiegaming@gmail.com>
2022-05-24 20:03:35 +0800

    added bugs

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import { UserContext } from "../UserContext";
+import Button from "./Button";
 
 function NavBar() {
-	return <div>NavBar</div>;
+	const { user, setUser } = useContext(UserContext);
+	const [userState, setUserState] = useState(false);
+
+	useEffect(() => {
+		if (user != null) {
+			setUserState(user.auth);
+		}
+	}, [user]);
+
+	return (
+		<div className="navContainer">
+			<div className="navDefault">
+				<NavLink to={"/"}>
+					<Button content="Home" classnames="btn btnNav" />
+				</NavLink>
+				<NavLink to={"/groups"}>
+					<Button content="Groups" classnames="btn btnNav" />
+				</NavLink>
+			</div>
+
+			{!userState ? (
+				<div className="navUser">
+					<NavLink to={"/login"}>
+						<Button content="Login" classnames="btn btnNav" />
+					</NavLink>
+					<NavLink to={"/signup"}>
+						<Button content="Sign Up" classnames="btn btnNav" />
+					</NavLink>
+				</div>
+			) : (
+				<div className="navUser">
+					<Button content="Profile" classnames="btn btnNav" />
+					<Button content="Sign Out" classnames="btn btnNav" />
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default NavBar;
