ffba9b5b006ad8b728bce1279a2a10bbbf1447b6
oldbettiegaming@gmail.com <oldbettiegaming@gmail.com>
2022-05-25 16:59:46 +0800

    added groups route

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -4,16 +4,18 @@ import Home from "./Home";
 import Login from "./Login";
 import SignUp from "./SignUp";
 import Profile from "./Profile";
+import Groups from "./Groups";
 
 function Pages() {
 	const location = useLocation();
 
 	return (
-		<Routes>
+		<Routes location={location} key={location.pathname}>
 			<Route path="/" element={<Home />} />
 			<Route path="/login" element={<Login />} />
 			<Route path="/signup" element={<SignUp />} />
 			<Route path="/user/:id" element={<Profile />} />
+			<Route path="/groups" element={<Groups />} />
 		</Routes>
 	);
 }
