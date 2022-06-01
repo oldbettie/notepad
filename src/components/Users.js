4f5c81a9a90e9d2b64267433ac40ec7ee5c5a2f5
oldbettiegaming@gmail.com <oldbettiegaming@gmail.com>
2022-06-01 08:37:00 +0800

    added invite inside page

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState, useContext } from "react";
+import { useParams } from "react-router-dom";
+
 import { FiUsers } from "react-icons/fi";
 import { UserContext } from "../UserContext";
 import styles from "./Users.module.scss";
 
 function Users({ userList }) {
+	let params = useParams();
+
 	const { user } = useContext(UserContext);
 	const [open, setOpen] = useState(false);
 	const [userCount, setUserCount] = useState(0);
@@ -27,7 +31,7 @@ function Users({ userList }) {
 			style={
 				!open
 					? { width: "40px", height: "40px" }
-					: { width: "180px", height: `${40 * userCount + 5}px` }
+					: { width: "180px", height: `${40 * userCount + 10}px` }
 			}>
 			{!open ? (
 				<div className={styles.userContentContainer}>
@@ -38,10 +42,6 @@ function Users({ userList }) {
 					className={styles.userContentContainerOpen}
 					style={{ height: `${40 * userCount}px` }}>
 					<div className={styles.allUsersContainer}>
-						<div className={styles.currentUser}>
-							<h6>{user.userName}</h6>
-							<FiUsers />
-						</div>
 						{users.map((user, index) => {
 							return (
 								<div className={styles.usersBox} key={index}>
@@ -50,6 +50,15 @@ function Users({ userList }) {
 								</div>
 							);
 						})}
+						<div
+							className={styles.inviteContainer}
+							onClick={() =>
+								navigator.clipboard.writeText(
+									`https://noteteams.netlify.app/subject/${params.id}`
+								)
+							}>
+							<h4 className={styles.inviteBtn}>Invite</h4>
+						</div>
 					</div>
 				</div>
 			)}
