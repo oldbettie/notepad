5f9659b7244e46575404085d1bb9a88a075adabb
oldbettiegaming@gmail.com <oldbettiegaming@gmail.com>
2022-06-09 10:12:06 +0800

    updated users for usability

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,28 +1,32 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
 import { FiUsers } from "react-icons/fi";
-import { UserContext } from "../UserContext";
 import styles from "./Users.module.scss";
 
 function Users({ userList }) {
 	let params = useParams();
-
-	const { user } = useContext(UserContext);
 	const [open, setOpen] = useState(false);
 	const [userCount, setUserCount] = useState(0);
-	const [users, setUsers] = useState([
-		{ userName: "bob" },
-		{ userName: "sarah" },
-		{ userName: "timmy" },
-	]);
+	const [users, setUsers] = useState([{}]);
+	const [copied, setCopied] = useState(0);
+
 	useEffect(() => {
 		setUserCount(users.length);
-	}, []);
+	}, [users]);
 
 	useEffect(() => {
 		setUsers(userList);
 	}, [userList]);
+
+	function copyClipboard() {
+		setCopied(1);
+		navigator.clipboard.writeText(
+			`https://noteteams.netlify.app/subject/${params.id}`
+		);
+		setTimeout(() => {
+			setCopied(0);
+		}, 3000);
+	}
 
 	return (
 		<div
@@ -31,8 +35,12 @@ function Users({ userList }) {
 			style={
 				!open
 					? { width: "40px", height: "40px" }
-					: { width: "180px", height: `${40 * userCount + 10}px` }
+					: { width: "180px", height: `${40 * userCount}px` }
 			}>
+			<div className={styles.copiedLink} style={{ opacity: copied }}>
+				<h4>Link copied!</h4>
+			</div>
+
 			{!open ? (
 				<div className={styles.userContentContainer}>
 					<FiUsers />
@@ -50,13 +58,7 @@ function Users({ userList }) {
 								</div>
 							);
 						})}
-						<div
-							className={styles.inviteContainer}
-							onClick={() =>
-								navigator.clipboard.writeText(
-									`https://noteteams.netlify.app/subject/${params.id}`
-								)
-							}>
+						<div className={styles.inviteContainer} onClick={copyClipboard}>
 							<h4 className={styles.inviteBtn}>Invite</h4>
 						</div>
 					</div>
